f0cf7d262c2b6d361da9cc128010bc7663044c15
Blair Guardia <blairguardia@gmail.com>
2021-06-03 16:45:19 +0900

    Add Users Controller and show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:show]
+  
   resources :events do
     resources :attendees, only: [:create, :destroy]
   end
