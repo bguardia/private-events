a9884819cf5f392a445ff68d6ca6280ddf145c2c
Blair Guardia <blairguardia@gmail.com>
2021-06-04 14:27:17 +0900

    Add ongoing scope to Events and update event views

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,26 @@ class Event < ApplicationRecord
     has_and_belongs_to_many :attendees, class_name: "User", join_table: "attended_events_attendees"
 
     scope :upcoming, ->{ where("events.start_date >= ?", DateTime.current) }
-    scope :past, ->{ where("events.start_date < ?", DateTime.current) }
+    scope :ongoing, ->{ where("events.start_date < :current_datetime AND events.end_date > :current_datetime", current_datetime: DateTime.current)}
+    scope :past, ->{ where("events.end_date < ?", DateTime.current) }
+
+    def pretty_start_date
+        self.start_date.strftime('%B %-d, %Y')
+    end
+
+    def start_time
+        self.start_date.localtime.strftime('%k:%M')
+    end
+
+    def upcoming?
+        start_date >= DateTime.current
+    end
+
+    def past?
+        start_date < DateTime.current
+    end
+
+    def ongoing?
+        start_date < DateTime.current && end_date > DateTime.current
+    end
 end
