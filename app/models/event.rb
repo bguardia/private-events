43d6b695ed156460469e8b4ccc67c7002c33b1f7
Blair Guardia <blairguardia@gmail.com>
2021-06-04 12:45:51 +0900

    Update seeds file

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
     belongs_to :host, class_name: "User"
     has_and_belongs_to_many :attendees, class_name: "User", join_table: "attended_events_attendees"
+
+    scope :upcoming, ->{ where("events.start_date >= ?", DateTime.current) }
+    scope :past, ->{ where("events.start_date < ?", DateTime.current) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# USERS
+puts "Creating users..."
+user_data = CSV.parse(File.open("users.csv").readlines, headers: true)
+user_data.each do |row|
+    User.create(row.to_h)
+end
+
+# EVENTS
+puts "Creating events..."
+hosts = User.find(1..10)
+current_date = DateTime.current
+hosts.each_with_index do |host, i|
+    5.times do |c|
+        host.hosted_events.create(title: "#{host.name}'s Weekly Get-Together",
+                                  body: dummy_text,
+                                  start_date: current_date - c*7 - i,
+                                  end_date: (current_date - c*7 - i).advance(hours: 2))
+    end
+end
+
+# ATTENDEES
+puts "Adding attendees..."
+events = Event.all
+users = User.all
+usr_count = users.size
+prng = Random.new
+events.each do |event|
+    rand_start = prng.rand(usr_count)
+    rand_end = rand_start + prng.rand(usr_count - rand_start)
+    event.attendees << users[rand_start, rand_end]
+end
+
+
+
+
