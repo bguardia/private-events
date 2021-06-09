f30d68fa16b2f44e4e4ba9c27dd26bc40463962a
Blair Guardia <blairguardia@gmail.com>
2021-06-09 21:22:06 +0900

    Add has_many :invitations to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :host, class_name: "User"
+    has_many :invitations, dependent: :destroy
     has_and_belongs_to_many :attendees, class_name: "User", join_table: "attended_events_attendees"
 
     scope :upcoming, ->{ where("events.start_date >= ?", DateTime.current) }
